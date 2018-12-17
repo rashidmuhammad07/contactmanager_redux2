4bec8997bdeaf21532a33523d3758fc05ae593eb
Muhammad Rashid <mrashid@ucdavis.edu>
2018-12-16 23:54:51 -0800

    update about page component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default () => {
+const About = () => {
   return (
     <div>
       <h1 className="display-4">About Contact Manager</h1>
@@ -9,3 +9,5 @@ export default () => {
     </div>
   );
 };
+
+export default About;
